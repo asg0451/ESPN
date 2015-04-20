f415c799a0f7f44b82399df0b06a09ca04d54eb8
Miles Frankel <miles.frankel@gmail.com>
2015-04-20 19:59:35 -0400

    what

--- src/DBGui.java
+++ src/DBGui.java
@@ -7,30 +7,30 @@ import javax.swing.*;
 public class DBGui {
 
     private static void createAndShowGUI() {
-	//Create and set up the window.
-	JFrame frame = new JFrame("HelloWorldSwing");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	//Add the ubiquitous "Hello World" label.
-	JLabel label;
-	try {
-	    label = new JLabel(rundb());
-	} catch (Exception e) { label  = new JLabel(e.toString());}
-	frame.getContentPane().add(label);
+        //Add the ubiquitous "Hello World" label.
+        JLabel label;
+        try {
+            label = new JLabel(rundb());
+        } catch (Exception e) { label  = new JLabel(e.toString());}
+        frame.getContentPane().add(label);
 
-	//Display the window.
-	frame.pack();
-	frame.setVisible(true);
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-	//Schedule a job for the event-dispatching thread:
-	//creating and showing this application's GUI.
-	javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	    public void run() {
-		createAndShowGUI();
-	    }
-	});
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 
     private static final String dbClassName = "com.mysql.jdbc.Driver";
@@ -38,37 +38,37 @@ public class DBGui {
       "jdbc:mysql://68.175.70.96/testing";
 
     public static String rundb () throws Exception {
-	Class.forName(dbClassName);
+        Class.forName(dbClassName);
 
-	Properties p = new Properties();
-	p.put("user","miles");
-	p.put("password","ESPN");
+        Properties p = new Properties();
+        p.put("user","miles");
+        p.put("password","ESPN");
 
-	Connection con = DriverManager.getConnection(CONNECTION,p);
+        Connection con = DriverManager.getConnection(CONNECTION,p);
 
-	Statement stmt = null;
-	String query = "select * from employees;";
+        Statement stmt = null;
+        String query = "select * from employees;";
 
-	stmt = con.createStatement();
-	ResultSet rs = stmt.executeQuery(query);
+        stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
 
-	ArrayList<Integer> ids = new ArrayList<Integer>();
-	ArrayList<String> names = new ArrayList<String>();
+        ArrayList<Integer> ids = new ArrayList<Integer>();
+        ArrayList<String> names = new ArrayList<String>();
 
-	while (rs.next()) {
-	    String name = rs.getString("first_name") + " " +
-		rs.getString("last_name");
-	    int id = rs.getInt("id");
-	    ids.add(id);
-	    names.add(name);
-	}
+        while (rs.next()) {
+            String name = rs.getString("first_name") + " " +
+                rs.getString("last_name");
+            int id = rs.getInt("id");
+            ids.add(id);
+            names.add(name);
+        }
 
-	String ret = "";
+        String ret = "";
 
-	for(int i=0; i< ids.size(); i++) {
-	    ret += (ids.get(i)+ " " + names.get(i));
-	}
-	con.close();
-	return ret + "hi";
+        for(int i=0; i< ids.size(); i++) {
+            ret += (ids.get(i)+ " " + names.get(i));
+        }
+        con.close();
+        return ret+ "hi";
     }
 }
