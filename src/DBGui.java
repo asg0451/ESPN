42f71dd7441bf8d8e60d36eccfad692dd130d67d
Miles Frankel <miles.frankel@gmail.com>
2015-04-26 15:07:45 -0400

    pms info

--- src/DBGui.java
+++ src/DBGui.java
@@ -28,72 +28,80 @@ public class DBGui {
 	private static String[] tournament_game_info;
 	private static JComboBox dropdown;
 
+	private static JButton pms_info_btn;
+	private static JTextField pms_info_pname;
+	private static JTextField pms_info_gnum;
+	private static JTextArea pms_info_res;
 
-    public static void main(String[] args) {
-	//Schedule a job for the event-dispatching thread:
-	//creating and showing this application's GUI.
-	javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	    public void run() {
-		createAndShowGUI();
-	    }
-	});
-    }
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
 
-    private static void createAndShowGUI() {
-	//Create and set up the window.
-	JFrame frame = new JFrame("Database Results");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	addComponentsToPane(frame.getContentPane());
-	//frame.getContentPane().add(label);
-	//Display the window.
-	frame.pack();
-	frame.setVisible(true);
-    }
-    public static void addComponentsToPane(Container contentPane) {
-	contentPane.setLayout(new GridLayout(0,3)); // 4 columns
-	//////Team Info row
-	contentPane.add(new JLabel("Team Info. Please enter team id."));
-	t_info_id = new JTextField();  // enter the id
-	contentPane.add(t_info_id);
+	private static void createAndShowGUI() {
+		//Create and set up the window.
+		JFrame frame = new JFrame("Database Results");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addComponentsToPane(frame.getContentPane());
+		//frame.getContentPane().add(label);
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public static void addComponentsToPane(Container contentPane) {
+		contentPane.setLayout(new GridLayout(0,4)); // 4 columns
+		//////Team Info row
+		contentPane.add(new JLabel("Team Info. Please enter team id."));
+		t_info_id = new JTextField();  // enter the id
+		contentPane.add(t_info_id);
 
-	t_info_btn = new JButton("Search Database");
-	t_info_btn.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			try {
+		t_info_btn = new JButton("Search Database");
+		t_info_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
 					JFrame results = new JFrame("Search Results");
 					results.setVisible(true);
 					Container results_con = results.getContentPane();
-				t_info_res.setText(teamStats(Integer.parseInt(t_info_id.getText())));
+					t_info_res.setText(teamStats(Integer.parseInt(t_info_id.getText())));
+					t_info_res.setEditable(false);
 					results_con.add(t_info_res);
 					results.pack();
-			} catch (Exception ex) {}
-		}
-	});
-	contentPane.add(t_info_btn);
-	t_info_res = new JTextArea(2,42);
-//        contentPane.add(t_info_res);
+				} catch (Exception ex) {}
+			}
+		});
+		contentPane.add(new JLabel("")); // dummy
+		contentPane.add(t_info_btn);
+		t_info_res = new JTextArea(2,42);
+		//        contentPane.add(t_info_res);
 
-	//////Player Info row
-	contentPane.add(new JLabel("Player Info. Please enter Player Name."));
-	p_info_name = new JTextField();  // enter the id
-	contentPane.add(p_info_name);
+		//////Player Info row
+		contentPane.add(new JLabel("Player Info. Please enter Player Name."));
+		p_info_name = new JTextField();  // enter the id
+		contentPane.add(p_info_name);
 
-	p_info_btn = new JButton("Search Database");
-	p_info_btn.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			try {
+		p_info_btn = new JButton("Search Database");
+		p_info_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
 					JFrame results = new JFrame("Search Results");
 					results.setVisible(true);
 					Container result_con = results.getContentPane();
-				p_info_res.setText(playerInfo(p_info_name.getText()));
+					p_info_res.setText(playerInfo(p_info_name.getText()));
+					p_info_res.setEditable(false);
 					result_con.add(p_info_res);
 					results.pack();
-			} catch (Exception ex) {}
-		}
-	});
-	contentPane.add(p_info_btn);
-	p_info_res = new JTextArea(2,42);
-//        contentPane.add(p_info_res);
+				} catch (Exception ex) {}
+			}
+		});
+		contentPane.add(new JLabel("")); // dummy
+		contentPane.add(p_info_btn);
+		p_info_res = new JTextArea(2,42);
+		//        contentPane.add(p_info_res);
 
 		///////Game Info row
 		contentPane.add(new JLabel("Game Info. Please enter game ID."));
@@ -108,30 +116,33 @@ public class DBGui {
 					results.setVisible(true);
 					Container result_con = results.getContentPane();
 					g_info_res.setText(matchInfo(Integer.parseInt(g_info_num.getText())));
+					g_info_res.setEditable(false);
 					result_con.add(g_info_res);
 					results.pack();
 				} catch (Exception ex) {}
 			}
 		});
+		contentPane.add(new JLabel("")); // dummy
 		contentPane.add(g_info_btn);
 		g_info_res = new JTextArea(2,42);
-//		contentPane.add(g_info_res);
+		//		contentPane.add(g_info_res);
 
 		///////Tournament Info
-	contentPane.add(new JLabel("Tournament Info. Please enter Tournament Name."));
-	tournament_info_name = new JTextField();  // enter the id
-	contentPane.add(tournament_info_name);
+		contentPane.add(new JLabel("Tournament Info. Please enter Tournament Name."));
+		tournament_info_name = new JTextField();  // enter the id
+		contentPane.add(tournament_info_name);
 
-	tournament_info_btn = new JButton("Search Database");
-	tournament_info_btn.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			try {
+		tournament_info_btn = new JButton("Search Database");
+		tournament_info_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
 
 					JFrame results = new JFrame("Search Results");
 					results.setVisible(true);
 					Container result_con = results.getContentPane();
 					tournament_info_res = new JTextArea();
-				tournament_info_res.setText(tournamentInfo(tournament_info_name.getText()));
+					tournament_info_res.setText(tournamentInfo(tournament_info_name.getText()));
+					tournament_info_res.setEditable(false);
 					result_con.add(tournament_info_res);
 					tournament_game_info = tournamentGameInfo(tournament_info_name.getText());
 					for (int i = 0; i < tournament_game_info.length; i++) {
@@ -151,87 +162,113 @@ public class DBGui {
 					});
 					result_con.add(dropdown);
 					results.pack();
-			} catch (Exception ex) {}
-		}
-	});
-	contentPane.add(tournament_info_btn);
-    }
+				} catch (Exception ex) {}
+			}
+		});
+		contentPane.add(new JLabel("")); // dummy
+		contentPane.add(tournament_info_btn);
 
-    public static String teamStats(int id) throws Exception {
-	Class.forName(dbClassName);
-	Properties p = new Properties();
-	p.put("user","miles");
-	p.put("password","ESPN");
-	Connection con = DriverManager.getConnection(CONNECTION,p);
+		///////PMS Info row
+		contentPane.add(new JLabel("Player Match Stats. Please enter Player name and game number"));
+		pms_info_pname = new JTextField(); //Enter game id
+		contentPane.add(pms_info_pname);
 
-	String team_info = "select * from Team where team_id = ?;";
-	PreparedStatement pstmt_tinfo = con.prepareStatement(team_info);
-	pstmt_tinfo.setInt(1,id);
+		pms_info_gnum = new JTextField(); //Enter game id
+		contentPane.add(pms_info_gnum);
 
-	ResultSet rs = pstmt_tinfo.executeQuery();
-	String ret = "";
-	while (rs.next())
-	     ret = rs.getString("team_id")+ " (" + rs.getString("name") + "), " +
-			 rs.getInt("rank");
-	ret +="\n";
-
-	String players =
-			"select * from Player P, Team T where T.team_id = ? and P.team_id = T.team_id;";
-	PreparedStatement pstmt_pinfo = con.prepareStatement(players);
-	pstmt_pinfo.setInt(1,id);
-
-	ResultSet rs2 = pstmt_pinfo.executeQuery();
-	while (rs2.next())
-	     ret += "\t" +rs2.getString("player_name") + ", " + rs2.getString("player_role") +
-				", " + rs2.getFloat("kdr") + " \n";
-
-	con.close();
-	return ret;
-    }
-
-    public static String playerInfo(String pname) throws Exception {
-	Class.forName(dbClassName);
-	Properties p = new Properties();
-	p.put("user","miles");
-	p.put("password","ESPN");
-	Connection con = DriverManager.getConnection(CONNECTION,p);
-
-	String query = "select * from Player P where P.player_name = ?;";
-	PreparedStatement pstmt = con.prepareStatement(query);
-	pstmt.setString(1,pname);
-
-	ResultSet rs = pstmt.executeQuery();
-	String ret = "";
-	while (rs.next()) {
-	     ret += "name: " + rs.getString("player_name") + "\n email: " +
-		rs.getString("email") + "\n role: " + rs.getString("player_role") +
-			"\n team_id: " + rs.getInt("team_id") + "\n kdr: " +
-				rs.getInt("kdr");
+		pms_info_btn = new JButton("Search Database");
+		pms_info_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					JFrame results = new JFrame("Search Results");
+					results.setVisible(true);
+					Container result_con = results.getContentPane();
+					pms_info_res.setText(pmsInfo(pms_info_pname.getText(), Integer.parseInt(pms_info_gnum.getText())));
+					pms_info_res.setEditable(false);
+					result_con.add(pms_info_res);
+					results.pack();
+				} catch (Exception ex) {}
+			}
+		});
+		contentPane.add(pms_info_btn);
+		pms_info_res= new JTextArea(2,42);
 	}
-	con.close();
-	return ret;
-    }
+
+	public static String teamStats(int id) throws Exception {
+		Class.forName(dbClassName);
+		Properties p = new Properties();
+		p.put("user","miles");
+		p.put("password","ESPN");
+		Connection con = DriverManager.getConnection(CONNECTION,p);
+
+		String team_info = "select * from Team where team_id = ?;";
+		PreparedStatement pstmt_tinfo = con.prepareStatement(team_info);
+		pstmt_tinfo.setInt(1,id);
+
+		ResultSet rs = pstmt_tinfo.executeQuery();
+		String ret = "";
+		while (rs.next())
+			ret = rs.getString("name") + " (" +  rs.getString("team_id") + "), " + " rank: "
+					+ rs.getInt("rank");
+		ret +="\n";
+
+		String players =
+				"select * from Player P, Team T where T.team_id = ? and P.team_id = T.team_id;";
+		PreparedStatement pstmt_pinfo = con.prepareStatement(players);
+		pstmt_pinfo.setInt(1,id);
+
+		ResultSet rs2 = pstmt_pinfo.executeQuery();
+		while (rs2.next())
+			ret += "\t" +rs2.getString("player_name") + ", " + rs2.getString("player_role") +
+			", " + rs2.getFloat("kdr") + " \n";
+
+		con.close();
+		return ret;
+	}
+
+	public static String playerInfo(String pname) throws Exception {
+		Class.forName(dbClassName);
+		Properties p = new Properties();
+		p.put("user","miles");
+		p.put("password","ESPN");
+		Connection con = DriverManager.getConnection(CONNECTION,p);
+
+		String query = "select * from Player P where P.player_name = ?;";
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setString(1,pname);
+
+		ResultSet rs = pstmt.executeQuery();
+		String ret = "";
+		while (rs.next()) {
+			ret += "name: " + rs.getString("player_name") + "\n email: " +
+					rs.getString("email") + "\n role: " + rs.getString("player_role") +
+					"\n team_id: " + rs.getInt("team_id") + "\n kdr: " +
+					rs.getInt("kdr");
+		}
+		con.close();
+		return ret;
+	}
 
 	public static String tournamentInfo(String tournamentName) throws Exception{
-	Class.forName(dbClassName);
-	Properties p = new Properties();
-	p.put("user","miles");
-	p.put("password","ESPN");
-	Connection con = DriverManager.getConnection(CONNECTION,p);
+		Class.forName(dbClassName);
+		Properties p = new Properties();
+		p.put("user","miles");
+		p.put("password","ESPN");
+		Connection con = DriverManager.getConnection(CONNECTION,p);
 
-	String query = "select * from Tournament T where T.tournament_name = ?;";
-	PreparedStatement pstmt = con.prepareStatement(query);
-	pstmt.setString(1,tournamentName);
+		String query = "select * from Tournament T where T.tournament_name = ?;";
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setString(1,tournamentName);
 
-	ResultSet rs = pstmt.executeQuery();
-	String ret = "";
-	while (rs.next()) {
-	     ret += "Tournament Name: " + rs.getString("tournament_name") + "\n Year: " +
-		rs.getString("tournament_year") + "\n Winning Team ID: " + rs.getInt("winning_team_id") +
-			"\n Held in: " + rs.getString("country");
-	}
-	con.close();
-	return ret;
+		ResultSet rs = pstmt.executeQuery();
+		String ret = "";
+		while (rs.next()) {
+			ret += "Tournament Name: " + rs.getString("tournament_name") + "\n Year: " +
+					rs.getString("tournament_year") + "\n Winning Team ID: " + rs.getInt("winning_team_id") +
+					"\n Held in: " + rs.getString("country");
+		}
+		con.close();
+		return ret;
 	}
 
 	public static String[] tournamentGameInfo(String tournamentName) throws Exception {
@@ -281,7 +318,7 @@ public class DBGui {
 		int team2 =  rs.getInt("team2_id");
 
 		ret += "\nMatch Number: " + rs.getString("game_number") + "\n Match Length: " + rs.getInt("length") + "\n In Tournament: " +
-		rs.getString("tournament_name");
+				rs.getString("tournament_name");
 
 		//look for the winner
 		String query2 = "select * from Team T where T.team_id = ?;";
@@ -315,5 +352,27 @@ public class DBGui {
 
 		con.close();
 		return ret;
+	}
+
+	public static String pmsInfo(String pname, int gnum) throws Exception{
+		Class.forName(dbClassName);
+		Properties p = new Properties();
+		p.put("user","miles");
+		p.put("password","ESPN");
+		Connection con = DriverManager.getConnection(CONNECTION,p);
+
+		String query = "select * from PlayerMatchStat where player_name = ? and game_number = ? ;";
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setString(1,pname);
+		pstmt.setInt(2,gnum);
+
+		ResultSet rs = pstmt.executeQuery();
+		String ret = pname + ":\n " +  "game_number | champion | kills | deaths | assists | creep_score \n";
+		while (rs.next()) {
+			ret += rs.getInt("game_number") + " | " + rs.getString("champion") + " | " + rs.getInt("kills")
+					+ " | " + rs.getInt("deaths") + " | " + rs.getInt("assists") + " | " + rs.getInt("creep_score");
 		}
+		con.close();
+		return ret;
+	}
 }
